7c4332527a6b094fc7797af711d06df8718a06d3
Peter Ho <maho@redhat.com>
2022-06-25 13:07:46 +0800

    Modified SQL script to insert sample data.

--- mysql-database-table-prep.sql
+++ mysql-database-table-prep.sql
@@ -1,6 +1,11 @@
+CREATE DATABASE mydatabase;
+
 CREATE TABLE customer_info(  
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
     clientNr VARCHAR(255) COMMENT 'Client Number'
     clientMembership VARCHAR(255) COMMENT 'Client Membership Level'
 ) DEFAULT CHARSET UTF8;
 
+INSERT INTO customer_info (clientNr, clientMembership) VALUES ('A9', 'GOLD');
+INSERT INTO customer_info (clientNr, clientMembership) VALUES ('B2', 'SILVER');
+INSERT INTO customer_info (clientNr, clientMembership) VALUES ('A4', 'NORMAL');
